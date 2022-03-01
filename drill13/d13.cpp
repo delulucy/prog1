93faaf05458787b23ca616e06aa02b86a288cc06
hallgato <iamrinaaaaa@gmail.com>
2022-03-01 08:31:33 +0100

    drill of chapter 13

---
+++ drill13/d13.cpp
@@ -0,0 +1,16 @@
+#include "Simple_window.h"
+#include "Graph.h"
+//g++ d13.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
+int main(){
+	using namespace Graph_lib;
+	
+	Point tl{100,100};
+	
+	Simple_window win{tl,600,400,"My window"};
+	
+	
+	
+	
+	
+	win.wait_for_button();
+}
