2e75337a9c2d03b535d02ea79bc078cf432b0ade
hallgato <iamrinaaaaa@gmail.com>
2022-03-01 08:44:56 +0100

    drill of chapter 13

--- drill13/d13.cpp
+++ drill13/d13.cpp
@@ -6,11 +6,21 @@ int main(){
 	
 	Point tl{100,100};
 	
-	Simple_window win{tl,600,400,"My window"};
+	Simple_window win{tl,800,1000,"My window"};
 	
+	int x_size = win.x_max();
+	int y_size = 800;
+	int x_grid=100;
+	int y_grid=100;
 	
+	Lines grid;
+	for(int x=x_grid; x<x_size; x+=x_grid)
+		grid.add(Point{x,0},Point{x,y_size});
 	
-	
+	for(int y = y_grid; y<=y_size; y+=y_grid)
+		grid.add(Point{0,y},Point{x_size,y});
+		
+	win.attach(grid);
 	
 	win.wait_for_button();
 }
