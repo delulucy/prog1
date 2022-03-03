2cde5d1e9573d7bd946abf86f6f29e448847c861
iamrinaaaaa <iamrinaaaaa@gmail.com>
2022-03-03 16:09:38 +0100

    finished drill 13

--- drill13/d13.cpp
+++ drill13/d13.cpp
@@ -4,10 +4,14 @@
 int main(){
 	using namespace Graph_lib;
 	
+	//1.
 	Point tl{100,100};
 	
 	Simple_window win{tl,800,1000,"My window"};
+
+	win.wait_for_button();
 	
+	//2.
 	int x_size = win.x_max();
 	int y_size = 800;
 	int x_grid=100;
@@ -20,38 +24,48 @@ int main(){
 	for(int y = y_grid; y<=y_size; y+=y_grid)
 		grid.add(Point{0,y},Point{x_size,y});
 
-		
-	Vector_ref<Rectangle> rects;
-			
-	int y_help=0;
-			
-	for(int x=0;x<x_size;x+=x_grid){
-		for(int y=y_help;y<y_help+1;y+=2)
-			rects.push_back(new Rectangle{Point(x,y),Point(x+x_grid,y+y_grid)});
-		y_help+=y_grid;
-		}
-		
-			
-	
-	for(int i=0;i<rects.size();++i){
-		rects[i].set_fill_color(Color::red);
-		rects[i].set_color(Color::invisible);
-		win.attach(rects[i]);
-		}
-
 	win.attach(grid);
-	
-	
+
 	win.wait_for_button();
-	
-	Image logo {Point{0,0},"kep.jpg"};
-	win.attach(logo);
-	
-	for(int y=0;y<y_size;y+=y_grid);
-	
+
+	//3.
+	Vector_ref<Rectangle> rects;		
+	for(int i=0; i< x_size; i+=x_grid){
+		rects.push_back(new Rectangle{Point{i,i},Point{i+x_grid,i+x_grid}});
+		rects[rects.size()-1].set_fill_color(Color::red);
+		rects[rects.size()-1].set_color(Color::invisible);
+		win.attach(rects[rects.size()-1]);
+	}	
+			
 	win.wait_for_button();
+
+	//4. 
+	Image img1 {Point{300,600},"image2.jpeg"};
+	Image img2 {Point{0,200},"image2.jpeg"};
+	Image img3 {Point{600,400},"image2.jpeg"};
 	
-	
-	
+	win.attach(img1);
+	win.attach(img2);
+	win.attach(img3);
+	win.wait_for_button();
+
+	//5.
+	Image eye {Point{0,0},"image.jpeg"};
+	int y_eyeTopLeft=0;
+	win.attach(eye);
+	win.wait_for_button();
+
+	for(int i= 1; i<9;++i){
+		for(int j=1;j<8;++j){
+			eye.move(100,0);
+			win.wait_for_button();
+		}
+		y_eyeTopLeft+=100;
+		if(y_eyeTopLeft==800)
+			break;
+		eye.move(-700,100);
+		win.wait_for_button();
+	}
+	win.wait_for_button();
 	
 }
