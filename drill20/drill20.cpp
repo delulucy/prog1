ee921a7a5c8552c947d3c6b2c77b20d21f76b67a
iamrinaaaaa <iamrinaaaaa@gmail.com>
2022-04-08 22:16:50 +0200

    finished

--- drill20/drill20.cpp
+++ drill20/drill20.cpp
@@ -1,31 +1,85 @@
 #include "std_lib_facilities.h"
 
-ostream& print_array(ostream& os, int a[]){
-	os << "Output of print_array: ";
-	for (int i=0; i<10; i++)
-		 os << a[i] << " ";
-	os<<"\n";
-	return os;
-}
+template<typename Iter1, typename Iter2>
+Iter2 m_copy(Iter1 f1, Iter1 e1, Iter2 f2)
+{
+	for (auto i=f1;i!=e1; i++)
+		{
+		*f2=*i;
+		f2++;
+		}
+		return f2;
+};
 
-ostream& print_vector(ostream& os, vector<int> v){
-	os << "Output of print_vector: ";
-	for( auto& a : v)
-		os << a << " ";
-	os<<"\n";
-	return os;
-}
+int main()
 
-int main(){
-	int array [10]= {0,1,2,3,4,5,6,7,8,9};
-	print_array(cout,array);
+{
+	int a[10]=  {0,1,2,3,4,5,6,7,8,9};
+	vector<int>v{0,1,2,3,4,5,6,7,8,9};
+	list<int>l  {0,1,2,3,4,5,6,7,8,9};
 	
-	vector<int> vect(10);
-	copy(array, array+10, vect.begin());
-	print_vector(cout,vect);
+	cout<<"Base numbers: "<<"{0,1,2,3,4,5,6,7,8,9}"<<endl;
 	
-	list<int> list(10);
-	copy(array,array+10, list.begin());
+	int a2[10]; m_copy(begin(a),end(a),begin(a2));
+	vector<int>v2=v;
+	list<int>l2=l; 
 	
+	cout<<"Increased array: ";
+
+	for (auto& x: a)
+		{
+		x+=2;
+		cout << x <<' ';
+		} 
+		cout<<endl; 
+
+	cout<<"Increased list: ";
+
+	for (auto& x: l)
+		{
+		x+=3;
+		cout << x <<' ';
+		} 
+		cout<<endl;
+	
+	cout<<"Increased vector: ";
+
+	for (auto& x: v)
+		{
+		x+=5;
+		cout << x <<' ';
+		} 
+		cout<<endl;
+	
+	cout<<"Copied increased array into base(second) vector: ";
+
+	m_copy(begin(a),end(a),begin(v2)); 
+
+	for (auto x: v2)
+		cout<<x<<' '; cout<<endl;
+
+	cout<<"Copied increased list into base(second) array: ";
+
+	m_copy(l.begin(),l.end(),begin(a2));
+
+	for (auto x: a2)
+		cout<<x<<' '; cout<<endl;
+
+	int element1=3, element2=27;  
+
+	auto elem1=find(v.begin(),v.end(),element1); 
+
+	if (elem1!=v.end()) 
+		cout<<element1<< " is at "<<distance(v.begin(),elem1)<<" index."<<endl;
+	else
+		cout<<element1<<" is not in increased the vector."<<endl;
+
+	auto elem2=find(l.begin(),l.end(),element2);
+
+	if (elem2!=l.end())
+		cout<<element2<< " is at "<<distance(l.begin(),elem2)<<" index."<<endl;
+	else
+		cout<<element2 <<" is not in increased the list."<<endl;
+
 	return 0;
 }
